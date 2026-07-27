59f8afcd8046f74ff0bdd2cc4f308d8c9fcff720
Frederick005 <ananya.frederick005@stud.fh-dortmund.de>
2026-07-27 21:05:21 +0200

    converted the base addresses to structures

---
+++ workspace_Wokwi/2_gpio_output/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/Learning/Embedded/Chip_Headers/CMSIS/Device/ST/STM32C0xx/Include/stm32c0xx.h \
+ C:/Learning/Embedded/Chip_Headers/CMSIS/Device/ST/STM32C0xx/Include/stm32c031xx.h \
+ C:/Learning/Embedded/Chip_Headers/CMSIS/Include/core_cm0plus.h \
+ C:/Learning/Embedded/Chip_Headers/CMSIS/Include/cmsis_version.h \
+ C:/Learning/Embedded/Chip_Headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Learning/Embedded/Chip_Headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Learning/Embedded/Chip_Headers/CMSIS/Include/mpu_armv7.h \
+ C:/Learning/Embedded/Chip_Headers/CMSIS/Device/ST/STM32C0xx/Include/system_stm32c0xx.h
+C:/Learning/Embedded/Chip_Headers/CMSIS/Device/ST/STM32C0xx/Include/stm32c0xx.h:
+C:/Learning/Embedded/Chip_Headers/CMSIS/Device/ST/STM32C0xx/Include/stm32c031xx.h:
+C:/Learning/Embedded/Chip_Headers/CMSIS/Include/core_cm0plus.h:
+C:/Learning/Embedded/Chip_Headers/CMSIS/Include/cmsis_version.h:
+C:/Learning/Embedded/Chip_Headers/CMSIS/Include/cmsis_compiler.h:
+C:/Learning/Embedded/Chip_Headers/CMSIS/Include/cmsis_gcc.h:
+C:/Learning/Embedded/Chip_Headers/CMSIS/Include/mpu_armv7.h:
+C:/Learning/Embedded/Chip_Headers/CMSIS/Device/ST/STM32C0xx/Include/system_stm32c0xx.h:
