833edcde77688bf618f4b4ad1c5c2d9fa37f94d7
Frederick005 <ananya.frederick005@stud.fh-dortmund.de>
2026-09-07 14:24:57 +0200

    USART Driver Cleaned up

---
+++ workspace_stm32f411/2_4_USART_driver_clean/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Include/core_cm4.h \
+ C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Include/cmsis_version.h \
+ C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Include/cmsis_compiler.h \
+ C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Include/cmsis_gcc.h \
+ C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Include/mpu_armv7.h \
+ C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/uart.h
+C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Include/core_cm4.h:
+C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Include/cmsis_version.h:
+C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Include/cmsis_compiler.h:
+C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Include/cmsis_gcc.h:
+C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Include/mpu_armv7.h:
+C:/Learning/Embedded/Chip_Headers_Stm32f411/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/uart.h:
